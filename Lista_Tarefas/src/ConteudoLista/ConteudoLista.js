739b87d5fe0039b2f3f6142b449d731a6753ed35
yasminregina44 <yasminregina44.2020@gmail.com>
2023-11-21 21:41:11 -0300

    teste

--- Lista_Tarefas/src/ConteudoLista/ConteudoLista.js
+++ Lista_Tarefas/src/ConteudoLista/ConteudoLista.js
@@ -8,17 +8,18 @@ export default function ConteudoLista({ navigation }) {
     const [nomeItem, setNomeItem] = useState("")
 
     const salvarNomeItem = async () => {
-        const itemTemporario = await AsyncStorage.getItem(metadata.LISTA.NOMELISTA);
-        const saveName = nomeItem || "";
-        let novaLista = JSON.parse(listaTemporaria);
-        novaLista.push(saveName);
-        await AsyncStorage.setItem(metadata.LISTA.NOMELISTA, JSON.stringify(novoItem));
+        const itemTemporario = await AsyncStorage.getItem(metadata.ITEM.NOMEITEM);
+        const saveItem = nomeItem || "";
+        let novoItem = JSON.parse(itemTemporario);
+        novoItem.push(saveItem);
+        await AsyncStorage.setItem(metadata.ITEM.NOMEITEM, JSON.stringify(novoItem));
         navigation.navigate("Home");
     }
 
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Adicionar Lista</Text>
+            <Text>Adicionar Item</Text>
             <TextInput placeholder='Digite um item da lista' value={nomeItem} onChangeText={setNomeItem} />
             <TouchableOpacity onPress={salvarNomeItem} >
                 <Text>Adicionar Lista</Text>
